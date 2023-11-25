46760f9c4ee1f97e0489c1c15105eb4c1e8771c5
heldawana <Heldawana9@gmail.com>
2023-11-25 20:41:08 +0700

    Penentu Jumlah Hari

---
+++ src/penentujumlahhari/PenentuJumlahHari.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penentujumlahhari;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PenentuJumlahHari {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    public void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
